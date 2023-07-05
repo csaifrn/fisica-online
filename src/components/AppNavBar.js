f238ac988fe92a3b5f5a25d809df18d53f246aeb
GABRIEL DOS SANTOS MOURA <g.moura@academico.ifrn.edu.br>
2023-07-05 15:23:26 -0300

    criando componente AppNavbar

---
+++ src/components/AppNavBar.js
@@ -0,0 +1,15 @@
+
+const AppNavBar = (props) => {
+    return (
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">               
+                <div class="d-flex">                  
+                    <button class="°-btn-hover" onclick="linkTermologia()">Termologia</button>
+                    <button class="outline-btn-hover" onclick="linkSaibaMais()">Saiba Mais</button>
+                </div>
+		    </div>
+        </nav>
+    );
+};
+
+export default AppNavBar;
