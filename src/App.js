546873f13c374d23ee5309d9d32e296e15995ae1
GABRIEL DOS SANTOS MOURA <g.moura@academico.ifrn.edu.br>
2023-10-10 15:32:18 -0300

    Criar o estado de atividade em CardCorpoAtividade

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import {AppNavBar, CardAtividade} from "./components";
+import { useState } from 'react'
+import { AppNavBar, CardAtividade } from "./components";
 /*import './css/lista01.css';*/
 
 function App() {
+  const [atividade] = useState({
+    titulo: 'Lista 02',
+    disciplina: 'Física',
+    descricao: 'Termologia - Calorimetria 01 - Lista 02',
+    nota_de_aula: 'http://???',
+    topico: 'termologia',
+    subtopico: 'calorimetria',
+    autor: {
+      nome: 'Geraldo',
+      data: '2023',
+    },
+    atividade: { proxima: null, anterior: null },
+  })
+
   return (
     <div id="app">
       <AppNavBar />
